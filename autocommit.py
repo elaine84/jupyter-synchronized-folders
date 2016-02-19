3b17b5c55f6311971d20a4cc9d00a1535fbef42e
YuviPanda <yuvipanda@gmail.com>
2016-02-18 17:27:33 -0800

    Autocommit

--- autocommit.py
+++ autocommit.py
@@ -7,7 +7,7 @@ import time
 import argparse
 
 def run_git_command(*command):
-    subprocess.check_call([
+    return subprocess.check_output([
         '/usr/bin/git',
     ] + list(command))
 
@@ -20,16 +20,23 @@ if __name__ == '__main__':
     run_git_command('commit', '--allow-empty', '-m', 'Starting autocommit with period %s seconds' % args.time)
 
     while True:
-        run_git_command('add', '--all', '.')
-        try:
-            run_git_command('commit', '-m', 'Autocommit')
-        except:
-            pass
+        if run_git_command('status', '-s'):
+            changed = True
+        else:
+            changed = False
+
+        if changed:
+            run_git_command('add', '--all', '.')
+            try:
+                run_git_command('commit', '-m', 'Autocommit')
+            except:
+                pass
 
         run_git_command('pull', '-r', '-s', 'recursive', '-Xours', 'origin', 'master')
 
-        try:
-            run_git_command('push', 'origin', 'master')
-        except:
-            continue
+        if changed:
+            try:
+                run_git_command('push', 'origin', 'master')
+            except:
+                continue
         time.sleep(float(args.time))
