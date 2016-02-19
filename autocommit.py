1eb70685757f54b9dd84dee1682fac44b3f162a5
YuviPanda <yuvipanda@gmail.com>
2016-02-18 16:34:17 -0800

    Autocommit at Thu Feb 18 16:34:17 PST 2016

--- autocommit.py
+++ autocommit.py
@@ -3,7 +3,9 @@
 Simple script that autocommits files being worked on to git.
 """
 import subprocess
+import time
 from datetime import datetime
+
 
 
 def run_git_command(*command):
@@ -14,10 +16,11 @@ def run_git_command(*command):
 
 
 if __name__ == '__main__':
-    run_git_command('add', '.')
-    try:
-        run_git_command('commit', '-m', 'Autocommit at %s' % datetime.now())
-    except:
-        pass
-    run_git_command('pull', '-r', '-s', 'recursive', '-Xours', 'origin', 'master')
-    run_git_command('push', 'origin', 'master')
+    while True:
+        run_git_command('add', '.')
+        try:
+            run_git_command('commit', '-m', 'Autocommit at %s' % datetime.now())
+        except:
+            pass
+        run_git_command('pull', '-r', '-s', 'recursive', '-Xours', 'origin', 'master')
+        run_git_command('push', 'origin', 'master')
