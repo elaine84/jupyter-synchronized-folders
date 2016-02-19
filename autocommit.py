92595332ed34cbb22527e82162d665a0b1936bbb
YuviPanda <yuvipanda@gmail.com>
2016-02-18 21:00:09 -0800

    Autocommit

--- autocommit.py
+++ autocommit.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
                 'pull',
                 '--rebase',
                 '--strategy', 'recursive',
-                '-Xours',
+                '--strategy-option', ''
                 'origin', 'master'
             )
         except Exception:
