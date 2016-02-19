7af11f393f8730ec319847ffdaa649106e1cfa12
YuviPanda <yuvipanda@gmail.com>
2016-02-18 16:12:40 -0800

    Autocommit at Thu Feb 18 16:12:40 PST 2016

--- autocommit.py
+++ autocommit.py
@@ -2,3 +2,10 @@
 """
 Simple script that autocommits files being worked on to git.
 """
+import subprocess
+
+
+def run_git_command(*command):
+    subprocess.check_output([
+        '/usr/bin/git',
+    ] + command)
