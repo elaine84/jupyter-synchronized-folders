d7d73da0a002e086369a3947e2c56ce50acd8b17
YuviPanda <yuvipanda@gmail.com>
2016-03-04 16:49:45 -0800

    Autocommit

--- autocommit.py
+++ autocommit.py
@@ -11,6 +11,34 @@ from tornado.gen import Task, Return, coroutine
 import tornado.process
 
 
+@coroutine
+def call_subprocess(cmd, stdin_data=None, stdin_async=False):
+    """
+    Wrapper around subprocess call using Tornado's Subprocess class.
+    """
+    stdin = STREAM if stdin_async else subprocess.PIPE
+
+    sub_process = tornado.process.Subprocess(
+        cmd, stdin=stdin, stdout=STREAM, stderr=STREAM
+    )
+
+    if stdin_data:
+        if stdin_async:
+            yield Task(sub_process.stdin.write, stdin_data)
+        else:
+            sub_process.stdin.write(stdin_data)
+
+    if stdin_async or stdin_data:
+        sub_process.stdin.close()
+
+    result, error = yield [
+        Task(sub_process.stdout.read_until_close),
+        Task(sub_process.stderr.read_until_close)
+    ]
+
+    raise Return((result, error))
+
+
 def run_git_command(*command):
     logging.info(' '.join([str(c) for c in command]))
     return subprocess.check_output([
